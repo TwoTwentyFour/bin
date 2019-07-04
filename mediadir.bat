3e54c6c41885ae43b0b0838ca837e4a4c78bfb05
twotwentyfour <twotwentyfour@protonmail.com>
2019-07-04 00:35:42 -0400

    Inital commit

---
+++ mediadir.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set /P DIRNAME=Enter the name of the event:
+
+md %DIRNAME%\Images %DIRNAME%\Videos %DIRNAME%\Images\Raw %DIRNAME%\Images\Instagram %DIRNAME%\Images\Jpeg %DIRNAME%\Images\Edit %DIRNAME%\Images\Prints %DIRNAME%\Videos\Raw %DIRNAME%\Videos\Edit %DIRNAME%\Videos\Cuts
+
+pause
